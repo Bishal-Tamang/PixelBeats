3a22aa982014d3936639f6145ae1918be3edaf5e
Bishal Tamang <tbishal078@gmail.com>
2023-12-02 18:03:25 +0545

    Minor UI fixes

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,70 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import Loader from "./Loader";
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      divRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+  if (data && !Array.isArray(data)) {
+    return <div>Error: Data is not in the expected format</div>;
+  }
+
+  if(data) {
+    console.log('The top play data is:', data);
+  }
+
+// useEffect(() => {
+//   console.log('The topPlays data is:', data);
+// }, []);
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-1">{/* {topP} */}</div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const options = {
 //     method: 'GET',
@@ -7,27 +7,28 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 //       'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
 //     }
 //   };
-  
+
 //   fetch('https://shazam.p.rapidapi.com/charts/track', options)
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
 export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam.p.rapidapi.com',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '21bbb84814msh7fae9c0ac82a3fcp145aa1jsna71c59479c94');
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "21bbb84814msh7fae9c0ac82a3fcp145aa1jsna71c59479c94"
+      );
 
-            return headers;
-        }
-    }),
-    endpoints: (builder) => ({
-        getTopCharts: builder.query({query: () => '/charts/track'}),
-    }),
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/track" }),
+  }),
 });
 
-export const {
-    useGetTopChartsQuery,
-} = shazamCoreApi;
+export const { useGetTopChartsQuery } = shazamCoreApi;

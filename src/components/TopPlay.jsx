60fe4241e06d0e80533c53429fdd7a6d98ba38fa
Bishal Tamang <tbishal078@gmail.com>
2023-12-02 19:12:02 +0545

    Added Top Charts & Top Artists

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,6 +12,42 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import Loader from "./Loader";
 
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick}) => {
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      <h3 className="font-bold text-base text-white mr-3">{i + 1}</h3>
+      <div className="flex flex-1 flow-row justify-between items-center">
+        <img 
+          className="w-20 h-20 rounded-lg"
+          src={song?.images?.coverart} 
+          alt={song?.title} 
+        />
+        <div className="flex flex-1 flex-col justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+            <p className="text-xl font-bold text-white">
+              {song?.title}
+            </p>
+          </Link>
+
+          <Link to={`/artists/${song.artists[0].adamid}`}>
+            <p className="text-base text-gray-300 mt-1">
+              {song?.subtitle}
+            </p>
+          </Link>
+        </div>
+      </div>
+
+      <PlayPause 
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        song={song}
+        handlePause={handlePauseClick}
+        handlePlay={() => handlePlayClick(song, i)}
+      />
+    </div>
+  );
+};
+
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -25,21 +61,9 @@ const TopPlay = () => {
     }
   });
 
-  if (data && !Array.isArray(data)) {
-    return <div>Error: Data is not in the expected format</div>;
-  }
+  const topPlays = data?.tracks?.slice(0, 5);
 
-  if(data) {
-    console.log('The top play data is:', data);
-  }
-
-// useEffect(() => {
-//   console.log('The topPlays data is:', data);
-// }, []);
-
-  const topPlays = data?.slice(0, 5);
-
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -61,7 +85,49 @@ const TopPlay = () => {
           </Link>
         </div>
 
-        <div className="mt-4 flex flex-col gap-1">{/* {topP} */}</div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard 
+              key={song.key} 
+              song={song} i={i} 
+              isPlaying={isPlaying} 
+              activeSong={activeSong} 
+              handlePauseClick={handlePauseClick} 
+              handlePlayClick={handlePlayClick}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView='auto'
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays?.map((song, i) => (
+            <SwiperSlide 
+              key={song?.key}
+              style={{width: '25%', height: 'auto'}}
+              className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img src={song?.images.background} alt='name' className="rounded-full w-full object-cover"/>
+              </Link>
+              </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
